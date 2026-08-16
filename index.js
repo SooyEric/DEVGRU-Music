2610bb3bb454ffe7ce85e5405be146610d263a3e
SooyEric <soooyeric@gmail.com>
2026-08-16 07:49:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -331,7 +331,7 @@ function createNowPlayingContainer(
 
           new ButtonBuilder()
             .setCustomId('back')
-            .setEmoji('<:left:1538541669509636197>')
+            .setEmoji('<:left:1538544846459899955>')
             .setStyle(ButtonStyle.Secondary)
             .setDisabled(disabled),
           
@@ -363,7 +363,7 @@ function createNowPlayingContainer(
           
           new ButtonBuilder()
             .setCustomId('stop')
-            .setEmoji('<:cancel:1538542260189139084>')
+            .setEmoji('<:cancel:1538544866659672144>')
             .setStyle(ButtonStyle.Danger)
             .setDisabled(disabled)
           )
