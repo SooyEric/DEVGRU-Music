6f5874b752b762518d86ca4a3f701d4e90c19669
SooyEric <soooyeric@gmail.com>
2026-08-16 08:44:05 -0600

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ module.exports = {
       {
         name: 'Main Node',
         host: 'lavalinkv4.serenetia.com',
-        port: 443,
+        port: 80,
         password: 'https://seretia.link/discord',
-        secure: true
+        secure: false
       }
     ]
   }
